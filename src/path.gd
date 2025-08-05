a58807832e00da2d8384c185d7aa6b29bb73a681
Samantha Doran <samanthadoran3@gmail.com>
2025-08-05 15:14:17 -0700

    Update path class to ensure if directions specified on creation.

--- src/path.gd
+++ src/path.gd
@@ -4,7 +4,28 @@ class_name Path
 
 enum Directions {NE, E, SE, SW, W, NW, ORIGIN, DESTINATION, UNCONNECTED}
 
-@export var path: Array[Directions] = [Directions.UNCONNECTED, Directions.UNCONNECTED]
+var path: Array[Directions] = [Directions.UNCONNECTED, Directions.UNCONNECTED]
+
+func _init(first_direction: Directions = Directions.UNCONNECTED, second_direction: Directions = Directions.UNCONNECTED):
+	path = [first_direction, second_direction]
+	if first_direction < Directions.ORIGIN and second_direction < Directions.ORIGIN:
+		normalize_path()
+
+func normalize_path():
+	# TODO(Samantha): This should set the connections in a canonical order.
+	# This should be such that if a corner crosses the directional origin
+	# ex: (NE, NW), the first should always be NE
+	
+	# If we're crossing the directional origin...
+	if path_distance() > 3:
+		var canon_first_dir = max(path[0], path[1])
+		var canon_second_dir = min(path[0], path[1])
+		path = [canon_first_dir, canon_second_dir]
+	else:
+		# Otherwise, the first connection is always the one numerically closer to NE
+		var canon_first_dir = min(path[0], path[1])
+		var canon_second_dir = max(path[0], path[1])
+		path = [canon_first_dir, canon_second_dir]
 
 func path_distance() -> int:
 	var first_direction = path[0]
